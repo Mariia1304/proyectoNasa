8f13792bd5476173d7e7a016cf337b76c903ce10
Mariia <maria.bookvoed@gmail.com>
2019-10-14 15:52:00 -0300

    boilerplate preliminar

---
+++ mapa.js
@@ -0,0 +1,5 @@
+var wwd = new WorldWind.WorldWindow("canvasOne");
+wwd.addLayer(new WorldWind.BMNGLandsatLayer());
+wwd.addLayer(new WorldWind.CompassLayer());
+wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
+wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
